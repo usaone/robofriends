f70dd16eb7e46fb2c7438930f977ce3280d74311
usaone <uddipmitra@gmail.com>
2018-08-15 23:00:31 -0400

    Apply destructuring on id field as well

--- src/Card.js
+++ src/Card.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const Card = (props) => {
-    const { name, email } = props;
+    const { id, name, email } = props;
     return (
         <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img alt='robots' src={`https://robohash.org/${props.id}?size=200x200`} />
+            <img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
