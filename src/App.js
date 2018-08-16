a3caa13fa9a5ccfa9b25d3edad36083a80af8d94
usaone <uddipmitra@gmail.com>
2018-08-16 01:38:06 -0400

    Add basic component for SearchBox

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import CardList from './CardList';
+import SearchBox from './SearchBox';
 import { robots } from './robots';
 
 const App = () => {
   return (
-    <CardList robots={robots}/>
+    <div>
+      <h1>Robofriends</h1>
+      <SearchBox />
+      <CardList robots={robots}/>
+    </div>
   );
 }
 
