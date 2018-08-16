852aa6216a538494de8e20f27ac84d7a130bc161
usaone <uddipmitra@gmail.com>
2018-08-15 23:35:36 -0400

    Add key for React (https://reactjs.org/docs/lists-and-keys.html#keys)

--- src/CardList.js
+++ src/CardList.js
@@ -3,7 +3,7 @@ import Card from './Card';
 
 const CardList = ({robots}) => {
   const cardComponent = robots.map((user, i) => {
-    return <Card id={robots[i].id} name={robots[i].name} email={robots[i].email}/>
+    return <Card key={i} id={robots[i].id} name={robots[i].name} email={robots[i].email}/>
   });
   return (
     <div>
