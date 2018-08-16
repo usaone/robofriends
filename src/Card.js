8d9d008fb62581183e1f4f401a9ece852b667617
usaone <uddipmitra@gmail.com>
2018-08-15 23:01:42 -0400

    Apply destructuring right at the input to the function

--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
-const Card = (props) => {
-    const { id, name, email } = props;
+const Card = ({ id, name, email }) => {
     return (
         <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
             <img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
