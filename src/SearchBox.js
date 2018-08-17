c4b9d99ecb8bb70dcb1472bf6fa24160752cf133
usaone <uddipmitra@gmail.com>
2018-08-17 07:26:23 -0400

    Add format and className to SearchBox

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 const SearchBox = () => {
   return (
-    <input type='search' placeholder='search robots' />
+    <div className='pa2'>
+      <input 
+        className='pa3 ba b--green bg-lightest-blue'
+        type='search' 
+        placeholder='search robots' 
+      />
+    </div>
   );
 }
 
